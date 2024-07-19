1343802bb4975db284c03314da69de63ea8f7084
MolchanovAlexander <rybkolhoz@gmail.com>
2024-07-19 09:36:12 +0300

    Initial commit

---
+++ src/main/java/org/example/testpbtask/TestPbTaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.testpbtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPbTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestPbTaskApplication.class, args);
+    }
+
+}
