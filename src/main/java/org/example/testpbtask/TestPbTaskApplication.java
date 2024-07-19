a5b1b94df713674e2165c0d47370c086765e7e6b
MolchanovAlexander <rybkolhoz@gmail.com>
2024-07-19 13:57:59 +0300

    undertow

--- src/main/java/org/example/testpbtask/TestPbTaskApplication.java
+++ src/main/java/org/example/testpbtask/TestPbTaskApplication.java
@@ -2,9 +2,16 @@ package org.example.testpbtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestPbTaskApplication {
+public class TestPbTaskApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TestPbTaskApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TestPbTaskApplication.class, args);
--- src/main/java/org/example/testpbtask/controller/OrderController.java
+++ src/main/java/org/example/testpbtask/controller/OrderController.java
@@ -6,6 +6,7 @@ import org.example.testpbtask.dto.CreateOrderRequestDto;
 import org.example.testpbtask.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,13 +26,12 @@ public class OrderController {
         orderService.placeOrder(requestDto);
     }
 
-    @PostMapping
-    @ResponseStatus(value = HttpStatus.CREATED)
+    @GetMapping
     public String hello(
             @RequestParam(value = "name", defaultValue = "Dno") String name,
             Model model
     ) {
         model.addAttribute("name", name);
-        return "hello";
+        return "view-books";
     }
 }
