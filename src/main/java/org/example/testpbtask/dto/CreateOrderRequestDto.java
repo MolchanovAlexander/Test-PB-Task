67343e82ba644e10400e75091a9f55b873524c96
MolchanovAlexander <rybkolhoz@gmail.com>
2024-07-22 18:53:10 +0300

    final commit after testing inputs

--- src/main/java/org/example/testpbtask/dto/CreateOrderRequestDto.java
+++ src/main/java/org/example/testpbtask/dto/CreateOrderRequestDto.java
@@ -2,17 +2,21 @@ package org.example.testpbtask.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateOrderRequestDto {
     @NotBlank
+    @NotNull
     @Size(min = 2, max = 55)
     private String lastName;
     @NotBlank
+    @NotNull
     @Size(min = 4, max = 55)
     private String productName;
     @Min(1)
+    @NotNull
     private Integer amount;
 }
